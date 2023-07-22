94cb1a42e60a923c72dc01aa8c7261b769353903
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-07-22 21:57:37 +0300

    Defined a few sequences and settings

--- scripts/sequence_generator.py
+++ scripts/sequence_generator.py
@@ -11,6 +11,46 @@ from traceback import format_exc
 # Given a list of sequence statements, generate those sequences
 
 
+# ======== SETTINGS ======== #
+# Length of the sequences
+sequence_length: Final[int] = 50
+# Number of places to round decimals to
+round_decimals: Final[int] = 6
+
+# Flag that dictates if the sequences should be print to console
+print_to_console: Final[bool] = True
+# Filename to print sequences to or None for no file output
+output_file: Final[str | None] = None
+
+# Format of the output for a single sequence. Supports {index} and {sequence}.
+sequence_format: Final[str] = "!{sequence}"
+# Separators between sequences and sequence elements
+sequence_separator: Final[str] = "\n\n"
+element_separator: Final[str] = ", "
+
+# Sequences are set in the targets selection below
+
+
+# ======== SEQUENCES ======== #
+def seq_constant(value: float) -> Iterable[float]:
+    while True:
+        yield value
+
+def seq_linear(start: float, shift: float) -> Iterable[float]:
+    val = start
+    while True:
+        yield val
+        val = val + shift
+
+
+# ======== TARGETS ======== #
+sequences: Final[list[Iterable[float]]] = [
+    seq_constant(value=5),
+    seq_linear(start=5, shift=1.05)
+]
+
+
+# ======== GENERATION ======== #
 def main():
     pass
 
