21e4d0b74c27d59fd231eb05aa5ec95431a9c161
Rephi <83082012+Rephidock@users.noreply.github.com>
2023-12-04 18:11:58 +0100

    TokkiGnerator: added settings including alphabet

--- scripts/color_tokki_svg_generator.py
+++ scripts/color_tokki_svg_generator.py
@@ -11,12 +11,86 @@ from traceback import format_exc
 # that text written using ColorTokki constructed script.
 #
 # Script info on Omniglot: https://www.omniglot.com/conscripts/colorhoney.php 
+#
+# Some alterations have been made to allow for spaces and
+# punctuation without breaking the flow of writing.
 
 
 # ======== SETTINGS ======== #
-# Filename to write resulting image into
-output_file: Final[str] = "generated_image.svg"
-# TODO: Add all other settings
+# Path to the file with text to spell out
+input_file: Final[str] = "script.txt"
+# Filename to write resulting image into.
+# Supports {name} for input file name without extension.
+output_file: Final[str] = "{name}.generated.svg"
+
+# The height of one letter including padding in pt
+letter_height: Final[int] = 10
+# Width of the image in the number of characters
+image_width: Final[int] = 10
+
+# The background color of the resulting image
+background_color: Final[str] = "white"
+# The color palette of svg (css) colors.
+# Each color is assigned a key to be used by glyphs in the alphabet.
+# A tuple of 2 colors can be given, then the first color is the fill and the second is outline.
+color_pallette: Final[dict[str, str | (str, str)]] = {
+    "R": "#FF0000", # Red
+    "G": "#19A319", # Green
+    "B": "#0167FF", # dark Blue
+    "S": "#00CCFF", # Sky blue
+    "V": "#CC66FF", # Violet
+    "Y": "#FFCE0C", # Yellow
+
+    "GA": "#7F7F7F", # Average Gray
+    "GB": "#303030", # darker (Blacker) Gray
+    "GW": ("#D1D1D1", "#303030"), # lighter (Whiter) Gray
+}
+# The color keys used for each letter in order of top, then bottom (light then dark)
+alphabet: Final[dict[str, (str, str)]] = {
+
+    # Vowels
+    'A': ("V", "V"),
+    'E': ("R", "R"),
+    'I': ("G", "G"),
+    'O': ("Y", "Y"),
+    'U': ("B", "B"),
+    'Y': ("S", "S"),
+
+    # Consonants
+    'B': ("V", "S"),
+    'C': ("V", "B"),
+    'D': ("V", "G"),
+    'F': ("R", "G"),
+    'G': ("R", "B"),
+    'H': ("R", "S"),
+    'J': ("G", "B"),
+    'K': ("G", "S"),
+    'L': ("G", "V"),
+    'M': ("G", "Y"),
+    'N': ("G", "R"),
+    'P': ("Y", "V"),
+    'Q': ("Y", "S"),
+    'R': ("Y", "B"),
+    'S': ("Y", "G"),
+    'T': ("Y", "R"),
+    'V': ("B", "S"),
+    'W': ("B", "V"),
+    'X': ("B", "Y"),
+    'Z': ("S", "B"),
+
+    # Extended
+    ' ': ("GW", "GW"),
+    '.': ("GW", "GB"),
+    "?": ("GA", "GB"),
+    '!': ("GB", "GB"),
+    '-': ("GA", "GA"),
+    ',': ("GW", "GA"),
+    "\"":("GA", "GW"),
+    "\'":("GA", "GW")
+    
+}
+# Set to true to treat whitespace characters as inline line breaks.
+space_breaks_word: Final[bool] = False
 
 
 # ======== DOCUMENT TEMPLATES ======== #
@@ -26,15 +100,25 @@ output_file: Final[str] = "generated_image.svg"
 # ======== GENERATION ======== #
 def main():
 
-    # Generate sequences
+    # Read input file
+    print("Reading input file...")
+    input_file_path = Path(input_file)
+    if not (input_file_path.exists() and input_file_path.is_file()):
+        print(f"File \"{input_file}\" does not exist or is not a file.")
+        return
+
+    text = input_file_path.read_text()
+
+    # Generate image
     print("Generating image...")
     image = ""
     # TODO: Generate image
 
     # Write to file
     print("Writing output...")
-    bytes_written = Path(output_file).write_text(image)
-    print(f"{bytes_written} bytes written to \"{output_file}\"")
+    output_file_path = Path(output_file.format(name=input_file_path.stem))
+    bytes_written = output_file_path.write_text(image)
+    print(f"{bytes_written} bytes written to \"{output_file_path}\"")
 
 
 if __name__ == "__main__":
